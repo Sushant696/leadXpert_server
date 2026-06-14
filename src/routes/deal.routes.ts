1f3963221a68ccf519e1f0a0b920e5c02b4fa3b6
sushant babu prasai <sushantprasaii7@gmail.com>
2026-06-14 14:00:00 +0545

    feat: deal routes with workspace middleware

---
+++ src/routes/deal.routes.ts
@@ -0,0 +1,48 @@
+import { Router } from "express";
+
+import {
+  checkWorkspaceMembership,
+  requiredCompanyRole,
+} from "../middlewares/hasPermission";
+import { Roles } from "../constants/roles";
+import { middlewares } from "../middlewares/isAuthenticated";
+import DealController from "../controller/deal.controller";
+
+const dealRouter = Router();
+const dealController = new DealController();
+
+// create deal
+dealRouter.post(
+  "/:workspaceId/deals",
+  middlewares.isAuthenticated,
+  checkWorkspaceMembership,
+  requiredCompanyRole([Roles.SUPER_ADMIN, Roles.ADMIN, Roles.AGENT]),
+  dealController.createDeal,
+);
+
+// get all deals
+dealRouter.get(
+  "/:workspaceId/deals",
+  middlewares.isAuthenticated,
+  checkWorkspaceMembership,
+  dealController.getDeals,
+);
+
+// get single deal
+dealRouter.get(
+  "/:workspaceId/deals/:dealId",
+  middlewares.isAuthenticated,
+  checkWorkspaceMembership,
+  dealController.getDealById,
+);
+
+// update deal
+dealRouter.patch(
+  "/:workspaceId/deals/:dealId",
+  middlewares.isAuthenticated,
+  checkWorkspaceMembership,
+  requiredCompanyRole([Roles.SUPER_ADMIN, Roles.ADMIN, Roles.AGENT]),
+  dealController.updateDeal,
+);
+
+export default dealRouter;
